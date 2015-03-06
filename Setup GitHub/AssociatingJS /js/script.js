3d824acbda3f4c475e1f243a53e04b713eb9b65a
Linda Pemberton <school@lanwaves.biz>
2015-03-05 21:26:31 -0500

    Add practice console.log with using alert
    Add What's under the hood.  Console.log year born and quote using alert
    prompt.

--- Setup GitHub/AssociatingJS /js/script.js
+++ Setup GitHub/AssociatingJS /js/script.js
@@ -5,4 +5,10 @@
 //Console.log practice video
 
 var yearBorn = 1965
+/*
+console.log ("Hello World"); //Check string variable
 console.log (yearBorn);
+*/
+
+alert ("Hello World!");// prints in box that pops up.
+alert ("yearBorn");   // this alert with quotes will make this type as a string rather than number.
