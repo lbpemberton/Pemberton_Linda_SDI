ca167e6ec747776b93f31fe144ec4021c3fd4d88
Linda Pemberton <school@lanwaves.biz>
2015-03-05 23:19:46 -0500

    Add comments needed to explain the number var and arrays
    Add comments needed to explain the number var and arrays.

--- Setup GitHub/Pemberton_Linda_Output_Assignment/js/script.js
+++ Setup GitHub/Pemberton_Linda_Output_Assignment/js/script.js
@@ -12,16 +12,17 @@ var numberChildren = 5; //number variable
 var numberGrandchildren = 4; //number variable
 
 
-console.log ("I have numerous children:");
-console.log (numberChildren);
+console.log ("I have numerous children:"); //print comment to explain number variable
+console.log (numberChildren);  //print number of children
 
-var childrenAges = ["16", "23", "25", "26", "27"];
-console.log ("Their ages vary:")
-console.log (childrenAges);
+var childrenAges = ["16", "23", "25", "26", "27"];  // array of children's ages
+console.log ("Their ages vary:");  //print comment to list children's ages vary
+console.log (childrenAges);  //print listing of children's ages
 
 
-console.log ("I also have several grandchildren:");
-console.log (numberGrandchildren);
+console.log ("I also have several grandchildren:"); //print comment to explain number variable of total number of
+    //grandchildren
+console.log (numberGrandchildren);  //print the number of grandchildren to console
 
 
 
