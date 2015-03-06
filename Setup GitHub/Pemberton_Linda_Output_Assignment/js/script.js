45f0366490d728410e8393ab35822f69ddeca717
Linda Pemberton <school@lanwaves.biz>
2015-03-05 23:26:23 -0500

    Add alert to beginning of Output Assignment
    Add alert to beginning of Output Assignment.  Explaining the reason for
    variables.

--- Setup GitHub/Pemberton_Linda_Output_Assignment/js/script.js
+++ Setup GitHub/Pemberton_Linda_Output_Assignment/js/script.js
@@ -8,6 +8,9 @@ March 5, 2015
 Creating Variables and Output
  */
 
+alert("This is about a proud mother and grandmother!!");//alert to show need for variables
+
+
 var numberChildren = 5; //number variable
 var numberGrandchildren = 4; //number variable
 
@@ -23,6 +26,8 @@ console.log (childrenAges);  //print listing of children's ages
 console.log ("I also have several grandchildren:"); //print comment to explain number variable of total number of
     //grandchildren
 console.log (numberGrandchildren);  //print the number of grandchildren to console
+
+
 
 
 
