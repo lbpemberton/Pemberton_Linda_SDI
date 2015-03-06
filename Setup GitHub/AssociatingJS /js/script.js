88950abfba19f9d496cf3e600f085ecd67b05602
Linda Pemberton <school@lanwaves.biz>
2015-03-05 22:17:51 -0500

    Add choice of one item in array.
    Add choice of one item in array.  The sequence that they follow from 0
    to …

--- Setup GitHub/AssociatingJS /js/script.js
+++ Setup GitHub/AssociatingJS /js/script.js
@@ -4,6 +4,12 @@
 //JavaScript
 //Arrays practice video
 
+//Meant to hold collections
+
 var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //Each item in this array has an index number
 
-console.log (muppetNames); //Gives complete array of names
+//console.log (muppetNames); //Gives complete array of names
+
+console.log(muppetNames[1]); // gives only 1 item in array with array starting at 0 to 3
+//0 = Kermit
+//1 = Miss Piggy and etc.
