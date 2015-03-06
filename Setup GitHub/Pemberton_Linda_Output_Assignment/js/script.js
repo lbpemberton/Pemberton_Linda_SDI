34e503ea0f4f38f3e496fa6515d6ac334021912a
Linda Pemberton <school@lanwaves.biz>
2015-03-05 23:13:07 -0500

    Setting up Creating Variables and Input
    Adding number variables and arrays.

---
+++ Setup GitHub/Pemberton_Linda_Output_Assignment/js/script.js
@@ -0,0 +1,28 @@
+/**
+ * Created by lindapembertonweb on 3/5/15.
+ */
+/*
+Linda Pemberton
+SDI section 02
+March 5, 2015
+Creating Variables and Output
+ */
+
+var numberChildren = 5; //number variable
+var numberGrandchildren = 4; //number variable
+
+
+console.log ("I have numerous children:");
+console.log (numberChildren);
+
+var childrenAges = ["16", "23", "25", "26", "27"];
+console.log ("Their ages vary:")
+console.log (childrenAges);
+
+
+console.log ("I also have several grandchildren:");
+console.log (numberGrandchildren);
+
+
+
+
