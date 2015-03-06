0a860ec19ebb85054089f62087270dba0a759b43
Linda Pemberton <school@lanwaves.biz>
2015-03-05 20:59:12 -0500

    Add Associating JS practice folder
    Add Associating JS practice folder - Beginning Javascript eBook Chapter
    2 sample

---
+++ Setup GitHub/AssociatingJS  practice/js/script.js
@@ -0,0 +1,27 @@
+
+//Linda Pemberton
+//SDI 02 Java Practice
+//March 5, 2015
+
+
+//alert("This is in the js file");  tested js connection
+
+var string1 = "Hello";      //assign variable
+var string2 = "Goodbye";    //assign second variable
+
+
+alert(string1);             //push alert box showing "Hello"
+alert(string2);             //push alert box showing "Goodbye"
+
+string2 = string1;          //change string2 to "Hello"
+
+alert (string1);            //push alert box showing "Hello"
+alert(string2);             //push alert box to = new string of "Hello"
+
+string1 = "Now for something different.";   //Change string1 to quote
+
+alert(string1);             //push alert to = new quote
+alert(string2);             //push alert to equal the above change to string2 = string1 "Hello"
+
+
+
