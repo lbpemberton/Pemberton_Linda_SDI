e5fdce89b0d0088e92d8a2a654fcd9cbcfb61f0a
Linda Pemberton <school@lanwaves.biz>
2015-03-06 21:42:32 -0500

    Add The Order of Operations examples
    Add the Order of Operations example (PEMDAS) with calculation of
    average and area of rectangle.

--- Setup GitHub/AssociatingJS /js/script.js
+++ Setup GitHub/AssociatingJS /js/script.js
@@ -2,13 +2,28 @@
 
 //Linda Pemberton
 //JavaScript
-//Expressions - Modulo Operator
+//Expressions - PEMDAS - The Order of Operations
 
+var quiz1 = 87;     //variable is quiz score
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
 
-var remainder = 32 % 10;    //10% of 32
+//the average is the sum of the items divided by the number of items
 
-console.log(remainder);     //print remainder only for 10% of 32
-        //remainder printed is only what is left over
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;  //addition must be placed in
+                //parenthesis because division comes first.
+
+console.log(average);   //print the average calculated
+
+//To calculate area of rectangle length x2 + width x2
+
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;  //multiplication is done first so parenthesis aren't needed.
+
+console.log(perimeter);
+
 
 
 
