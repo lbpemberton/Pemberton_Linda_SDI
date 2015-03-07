2477aed20326a0a710ed220d452ccb918f4990f4
Linda Pemberton <school@lanwaves.biz>
2015-03-06 20:01:52 -0500

    Add comments and correct age calculation
    Add comments and correct age calculation to expressions exercise.

--- Setup GitHub/AssociatingJS /js/script.js
+++ Setup GitHub/AssociatingJS /js/script.js
@@ -6,9 +6,10 @@
 
 
 
-var yearBorn = 1965;
+var yearBorn = 1965;    //add variable of year born
 
-var age = 2015 - yearBorn;
+var age = 2015 - yearBorn - 1;  // calculate age by current year and deduct year born
+                                // subtract 1 year when birthday hasn't passed.
 
 console.log(age);
 
