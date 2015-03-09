6ee2ea702c34f412b97ae2ffaba3fe62f33e4553
Linda Pemberton <school@lanwaves.biz>
2015-03-08 22:40:15 -0400

    Add Prompt and Alert example
    Add Prompt and Alert example from video.  Week 2 of SDI.

--- Setup GitHub/AssociatingJS /js/script.js
+++ Setup GitHub/AssociatingJS /js/script.js
@@ -2,17 +2,13 @@
 
 //Linda Pemberton
 //JavaScript
-//Expressions - Assignment Operators
+//Prompt and Alert
 
-//Calculating with AND assigning all on one line with these handy programming
-//shortcut operators.
+var yearBorn = prompt("Enter your year of birth:");  //Prompts user for variable information.
 
-var a = 3;  //Assignment operator is =.
-a ++;     //Assignment is a = a + 1 // a += 1
+console.log (yearBorn);  //Will print variable yearBorn input.
 
-//Note storage container is always on the left.
 
-console.log(a);
 
 
 
