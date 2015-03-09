eee3f8cd718fc20a79da39ae2873afcada19be8b
Linda Pemberton <school@lanwaves.biz>
2015-03-08 22:17:02 -0400

    Using assignment operator ++ as shortcut for +1.  Week 2 SDI video example.
    Using assignment operator ++ as shortcut for +1.  Week 2 SDI video
    example.

--- Setup GitHub/AssociatingJS /js/script.js
+++ Setup GitHub/AssociatingJS /js/script.js
@@ -8,7 +8,7 @@
 //shortcut operators.
 
 var a = 3;  //Assignment operator is =.
-a -- ;     //Assignment is a = a - 1 // a -= 1
+a ++;     //Assignment is a = a + 1 // a += 1
 
 //Note storage container is always on the left.
 
