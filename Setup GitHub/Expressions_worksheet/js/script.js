6b2d9107fe582a7a681509da3961f023460c0993
Linda Pemberton <school@lanwaves.biz>
2015-03-11 19:26:23 -0400

    Add dog years problem to Expressions Worksheet
    Add dog years problem to Expressions Worksheet.  Week 2 Assignment for
    Expressions_Worksheet.

--- Setup GitHub/Expressions_worksheet/js/script.js
+++ Setup GitHub/Expressions_worksheet/js/script.js
@@ -1,10 +1,27 @@
 
-/*
-Linda Pemberton
-March 9, 2015
-Expressions Worksheet
+/**
+*Linda Pemberton
+*March 9, 2015
+*Expressions Worksheet
 
-\*
+*/
+
+//alert("JavaScript works!"); Testing connection to html file.
+
+
+var actualAge = 3;      //Number variable for Sparky's age in human years.
+
+const dogYears = 7;     //Establish constant of dog years.
+
+
+var dogAge = actualAge * dogYears;  //Variable to multiply actual age times dog years.
+
+console.log ("Sparky is " + actualAge + " human years old which is " + dogAge + " in dog years.");
+//Print result of Sparky's actual age and what that calculates to in dog years.
+
+
+
+
 
 
 
