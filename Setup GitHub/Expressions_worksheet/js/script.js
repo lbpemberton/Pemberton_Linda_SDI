ed5aac9edb90bb4d5ebc3e2fe745d1c8f23a2a90
Linda Pemberton <school@lanwaves.biz>
2015-03-12 18:21:50 -0400

    Add Slice of Pie Part I to Expressions Worksheet
    Add Slice of Pie Part I to Expressions Worksheet.  Calculation of
    multiplication and division.

--- Setup GitHub/Expressions_worksheet/js/script.js
+++ Setup GitHub/Expressions_worksheet/js/script.js
@@ -20,7 +20,18 @@ console.log ("Sparky is " + actualAge + " human years old which is " + dogAge + 
 //Print result of Sparky's actual age and what that calculates to in dog years.
 
 
+//Add Expression Problem - Slice of Pie Part I PEMDAS
 
+var slicesPerPizza  = 8;    //Number variable of slices per pizza.
+var partyPeople     = 24;   //Number variable of  people at party.
+var piesOrdered     = 12;    //Number variable of pizza pies ordered.
+
+var slicesEaten     = slicesPerPizza * piesOrdered / partyPeople; //Number variable equation  to determine
+                            //total slices eaten.
+
+
+//Print total of slices eaten per person.
+console.log ("Each person ate " + (slicesEaten) + " slices of pizza at the party.");
 
 
 
