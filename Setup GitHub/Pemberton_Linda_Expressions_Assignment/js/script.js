35695d6c47b9c05b6a99d4eb3f59d86a3a4d7ff5
Linda Pemberton <school@lanwaves.biz>
2015-03-13 16:08:13 -0400

    Add Expressions Assignment with variables
    Add Files for Expressions Assignment with variables used.

---
+++ Setup GitHub/Pemberton_Linda_Expressions_Assignment/js/script.js
@@ -0,0 +1,28 @@
+alert("This is in the js file");
+
+/**
+ Linda Pemberton
+ March 12, 1015
+ SDI - Section 02
+ Expressions Assignment
+ */
+
+//Establish variables
+var foster          = "foster children";
+var ages            = [3, 1, 15, 13, 16, 17, 4, 6, 18];
+var female          = "5";
+var male            = "4";
+
+
+var youngestChild, oldestChild, countyLicense;
+
+
+//Establish prompts
+youngestChild       = prompt("Please enter youngest child's name:");
+
+oldestChild         = prompt("Please enter oldest child's name:");
+
+countyLicense       = prompt("What county do you foster in?");
+
+
+
