733fcacdce43427bc56628dc476ab076539a4ba8
Linda Pemberton <school@lanwaves.biz>
2015-03-13 18:28:52 -0400

    Correct console.log requirements and align comments
    Correct console.log requirements and align comments.  Included list of
    arrays and comments lined up.

--- Setup GitHub/Pemberton_Linda_Expressions_Assignment/js/script.js
+++ Setup GitHub/Pemberton_Linda_Expressions_Assignment/js/script.js
@@ -14,10 +14,10 @@ var female          = 5;                                //List total females fos
 var male            = 4;                                //List total males fostered.
 var totalAges       = ages[0] + ages[1] + ages[2] + ages[3] + ages[4] + ages[5] + ages[6] + ages[7] +
                     ages[8];                            //Add ages of children fostered.
-var totalChildren   = female + male;                   //Add total number of children fostered.
-//Number(totalChildren) ;                     //Divide ages of children by number fostered to get average.
+var totalChildren   = female + male;                    //Add total number of children fostered.
 
-var avgChildren = totalAges / totalChildren;
+
+var avgChildren     = totalAges / totalChildren;        //Divide ages of children by number fostered to get average.
 
 var youngestChild, oldestChild, countyLicense;          //Establish variable names for prompts.
 
@@ -29,16 +29,26 @@ youngestChild       = prompt("Please enter youngest child's name:");  //Prompt n
 
 oldestChild         = prompt("Please enter oldest child's name:");    //Prompt name for oldest child.
 
-countyLicense       = prompt("What county do you foster in?");        //Prompt name of County fostered in.
+countyLicense       = prompt("What county do you foster in? \nYou do not need to type the word County."); //Prompt name
+// of County fostered in.
 
 
 
 
 //Establish output
-console.log("I love to " + "foster"  + " in " + countyLicense + "."); //Print what this is about and where it took
-// place.
 
-console.log("I fostered a variety of ages from " + ages[1] + " to "+ ages[8] + ".  Total ages of children are "
-+ totalAges + ".  The average age of children fostered was " + totalChildren + ".");
+
+//Divide ages of children by number fostered to get average.
+console.log("I love to " + "foster"  + " in " + countyLicense + " County.");
+
+
+//Print youngest age to highest and print average age fostered.
+console.log("I fostered a variety of ages from " + ages[1] + " to "+ ages[8] + ".  The list of ages were: " + ages +
+".  The average age of children fostered was " + totalChildren + ".");
+
+console.log("My youngest foster child's name was " + youngestChild + " and the oldest child's name was " +
+oldestChild + ".");  //Print prompted youngest name of child and oldest child's name.
+
+
 
 
