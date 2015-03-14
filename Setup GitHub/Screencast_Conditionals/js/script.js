f7f1d39cfdd837aa46087f78c0ec7e540a153c06
Linda Pemberton <school@lanwaves.biz>
2015-03-14 19:03:35 -0400

    Add else statement to If and else practice
    Add else statement to If and else practice.  If and Else screencast
    training SDI week 3.

--- Setup GitHub/Screencast_Conditionals/js/script.js
+++ Setup GitHub/Screencast_Conditionals/js/script.js
@@ -118,12 +118,18 @@ var minHeight = 48;
 
 //if the child is old enough, print to the console "you can ride".
 //if the kid is over 48 inches in height.
-if(kidHeight > minHeight){
+if(kidHeight > minHeight) {
     //code performed if condition is true
     console.log("you can ride the coaster!");
 
-}
-if(kidHeight <= minHeight){
+
+//if (kidHeight <= minHeight){
+    //code performed if condition is false.
+
+//Easier way to code without as many conditionals.
+}else{
+
+    //code performed if condition is false.
     console.log ("Sorry kid, you've got some growing to do first!");
-    
+
 }
