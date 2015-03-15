f18e6d8af908fe9d55ba912348f4686a2df5d354
Linda Pemberton <school@lanwaves.biz>
2015-03-14 20:06:15 -0400

    Add example for "else if" video
    Add example for "else if" video.  Choosing between three or more blocks
    of code in kid height scenario.

--- Setup GitHub/Screencast_Conditionals/js/script.js
+++ Setup GitHub/Screencast_Conditionals/js/script.js
@@ -109,12 +109,15 @@ if(kidHeight + 2 > minHeight){ //add two for sneaker height
 /**
  * Screencast: If and else.
  * Choosing between two blocks of code.
+ * Add "esle if" video -
+ * Choosing between three or more blocks of code.
  */
 
 //Using same conditional statement from above.
 
 var kidHeight = 48;
 var minHeight = 48;
+var wParentHeight = 45; //the height of the kid with the parent.
 
 //if the child is old enough, print to the console "you can ride".
 //if the kid is over 48 inches in height.
@@ -127,9 +130,18 @@ if(kidHeight > minHeight) {
     //code performed if condition is false.
 
 //Easier way to code without as many conditionals.
-}else{
 
-    //code performed if condition is false.
-    console.log ("Sorry kid, you've got some growing to do first!");
+}else if(kidHeight > wParentHeight) {
+
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present.");
+}else{
+    //sorry you have growing to do.
+    console.log("Sorry kid, you've got some growing to do first.");
 
 }
+/**
+ Screencast: else if video
+ Choosing between three or more blocks of code.
+ */
+
