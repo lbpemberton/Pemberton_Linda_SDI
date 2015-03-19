912fc68bdf78bb4b8989f1d41f7460a683fef6bb
Linda Pemberton <school@lanwaves.biz>
2015-03-18 23:59:45 -0400

    Add Group 1 example of celsius to fahrenheit converter 32F to 0C
    Add Group 1 example of celsius to fahrenheit converter 32F to 0C of
    Expressions with Conditionals.

--- Setup GitHub/Conditionals_worksheet/js/script.js
+++ Setup GitHub/Conditionals_worksheet/js/script.js
@@ -1,2 +1,35 @@
-alert("This is in the js file");
+//alert("This is in the js file");
 
+
+/**
+ Linda Pemberton
+ SDI Section 02
+ March 18, 2015
+ Conditionals Worksheet
+ */
+
+//Group 1:  Expressions with Conditionals
+
+//Data set to test is 32F is 0C
+
+
+var temp = 32;
+var unit = "F";
+
+
+if (unit === "F"){  //convert Fahrenheit to Celsius
+    var fahrenheitToCelsius    = (temp - 32) * 5/9;
+
+
+
+    console.log("The temperature is " + fahrenheitToCelsius + " degrees Celsius.")
+
+
+}else{
+
+    var celsiusToFahrenheit    = ((temp * 9/5) + 32);
+
+    console.log("The temperature is " + celsiusToFahrenheit + " degrees Fahrenheit.")
+
+
+}
