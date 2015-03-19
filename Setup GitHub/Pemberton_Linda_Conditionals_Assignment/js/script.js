050811e6539c36b5a5962d18a71dfd21dbbb8cb4
Linda Pemberton <school@lanwaves.biz>
2015-03-19 18:07:31 -0400

    Commit Variables into Conditionals Assignment
    Commit Variables into Conditionals Assignment and output to console in
    statements.

--- Setup GitHub/Pemberton_Linda_Conditionals_Assignment/js/script.js
+++ Setup GitHub/Pemberton_Linda_Conditionals_Assignment/js/script.js
@@ -1,1 +1,22 @@
-alert("Confirmed js file is connected!");
+//alert("Confirmed js file is connected!");
+
+/**
+ Linda Pemberton
+ SDI Section 02 week 3
+ March 19, 2015
+ Conditionals Assignment
+ */
+
+var cityState       = "Wilmington, NC";         //String variable of city and state.
+var milesPerTank    = 396;                      //Number variable of miles to tank of gas.
+var myCar           = "Toyota \"Highlander\"";  //String variable with escapes of type vehicle driven.
+var milesToNagsHead = 418;                      //Number variable of miles from Wilmington to Nags Head.
+var favVacNagsHead  = true;                     //Boolean that favorite vacation spot is Nags Head, NC.
+
+//Output above variables to console.
+console.log("I live in " +cityState+ ".");
+console.log("My " +myCar+ " averages " +milesPerTank+ " miles per tank.");
+console.log("It is " +milesToNagsHead+ " miles to Nags Head, NC.");
+console.log("It is " +favVacNagsHead+ " that my favorite vacation spot is Nags Head!");
+
+
