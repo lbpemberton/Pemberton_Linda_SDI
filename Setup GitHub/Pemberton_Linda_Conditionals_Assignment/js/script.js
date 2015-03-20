797f6ee5c7da6b3d161d88031d8ed9f839cfbf6c
Linda Pemberton <school@lanwaves.biz>
2015-03-19 20:26:58 -0400

    Add prompts for two true statements and a math calc for end result
    Add prompts for two true statements and a math calc for end result.
    Taking Conditional Assignment through all true statements.

--- Setup GitHub/Pemberton_Linda_Conditionals_Assignment/js/script.js
+++ Setup GitHub/Pemberton_Linda_Conditionals_Assignment/js/script.js
@@ -19,4 +19,33 @@ console.log("My " +myCar+ " averages " +milesPerTank+ " miles per tank.");
 console.log("It is " +milesToNagsHead+ " miles to Nags Head, NC.");
 console.log("It is " +favVacNagsHead+ " that my favorite vacation spot is Nags Head!");
 
+//Confirm main statement with string message:
+favVacNagsHead      = confirm("Are you still planning your vacation in Nags Head, NC?\nPlease click OK for yes" +
+" or Cancel for no.");
+
+if (favVacNagsHead === true){
+
+    //Code to run if the vacation is going to be in Nags Head, NC.
+    var carDriven     = confirm("Are you driving the " +myCar+ " ?");
+
+    if (carDriven === true){                    //If car being driven is true.
+
+    //Print affirmation that the car being driven is myCar.
+    console.log("I will be taking my " +myCar+ " to Nags Head, N.C. for vacation.");
+
+        var tanksUsed = Number(milesToNagsHead * 2) / milesPerTank;
+        console.log("My trip to Nags Head, N.C. and back home took " + parseFloat(tanksUsed) + " tanks of gas.")
+
+
+
+
+
+
+    }
+
+
+
+
+}
+
 
