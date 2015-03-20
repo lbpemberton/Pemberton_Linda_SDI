926d0ba3378645e33192ca8eede4d348b121139f
Linda Pemberton <school@lanwaves.biz>
2015-03-19 23:29:31 -0400

    Complete conditionals after choosing true to going to Nags Head
    Complete conditionals after choosing false to going to Nags Head.

--- Setup GitHub/Pemberton_Linda_Conditionals_Assignment/js/script.js
+++ Setup GitHub/Pemberton_Linda_Conditionals_Assignment/js/script.js
@@ -34,11 +34,68 @@ if (favVacNagsHead === true){
     console.log("I will be taking my " +myCar+ " to Nags Head, N.C. for vacation.");
 
         var tanksUsed = Number(milesToNagsHead * 2) / milesPerTank;
-        console.log("My trip to Nags Head, N.C. and back home took " + parseFloat(tanksUsed) + " tanks of gas.")
+        console.log("My trip to Nags Head, N.C. and back home took " + parseFloat(tanksUsed) + " tanks of gas.");
+
+
+    }else{
+        //Change miles per tank to match number entered from prompt.
+
+        milesPerTank = prompt("How many miles per tank will this vehicle average?");
+
+        //Verify that an amount is entered in the prompt.
+        if (milesPerTank === ""){
+            milesPerTank = prompt("Please enter the amount of miles per tank that this vehicle will average:")
+        }
+        tanksUsed = Number(milesToNagsHead * 2) / milesPerTank;
+        console.log("My trip to Nags Head, N.C. and back home took " + parseFloat(tanksUsed) + " tanks of gas.");
+
+    }
 
 
 
 
+}else{
+    //Since I am not going to Nags Head, prompt for location of vacation.
+    var vacCity     =   prompt("Please enter city of vacation spot:");
+
+    //If user does not enter a City, prompt again for information.
+    if (vacCity === ""){
+
+        vacCity = prompt("Oops! You didn't enter the City of vacation spot.  Please enter:");
+
+
+        var vacState = prompt("Please enter State of vacation spot:");
+        if (vacState === "") {
+
+
+            vacState = prompt("Oops!  You didn't enter the State of vacation spot.  Please enter:");
+            //Print result of new vacation destination in console with use of string concatenation.
+        console.log("Instead of Nags Head, I will be going to " + vacCity + ", " + vacState + ".")}
+
+
+        myCar = confirm("Are you driving your Toyota \"Highlander\"? \n Click Ok for yes or Cancel for no.");
+
+        if (myCar === true) {
+
+            var milesToVac = prompt("How many miles to " + vacCity + ", " + vacState + "?");
+            if (milesToVac === "") {
+                milesToVac = prompt("Make sure you enter the amount of miles to your vacation spot:");
+            } else {
+                milesToVac = Number(milesToVac * 2) / milesPerTank;
+            }
+
+            console.log("This vacation round-trip will require " + milesToVac + " tanks of gas.");
+
+        }else{
+            var truck;
+            var suv;
+
+            carDriven = confirm("Click OK if truck or suv.  Click Cancel if automobile.")
+            if(carDriven === truck || suv);
+            console.log("You will use more gas than normal.");
+
+
+        }
 
 
     }
@@ -46,6 +103,5 @@ if (favVacNagsHead === true){
 
 
 
+
 }
-
-
