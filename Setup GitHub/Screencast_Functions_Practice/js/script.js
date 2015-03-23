5475c54adc82c90ea7a59d093f986f373fd49be9
Linda Pemberton <school@lanwaves.biz>
2015-03-23 19:42:33 -0400

    Add a Functions Basic Structure video example
    Add a Functions Basic Structure video example.  Functions Screencast.

--- Setup GitHub/Screencast_Functions_Practice/js/script.js
+++ Setup GitHub/Screencast_Functions_Practice/js/script.js
@@ -1,1 +1,20 @@
-alert("JS is connected!");
+//alert("JS is connected!");
+
+/**
+ Linda Pemberton
+ SDI Section 02
+ March 23, 2015
+ Functions Screencast Practice
+ */
+
+//Functions - A Basic Function
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width   =   20;
+    var height  =   30;
+    var area    =   width * height;
+    console.log(area);
+}
