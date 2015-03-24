00be75673fbde2832a45e3c4bef2694b13ce41e6
Linda Pemberton <school@lanwaves.biz>
2015-03-24 14:49:08 -0400

    Add function for dog years example of Arguments & Parameters
    Add function for dog years example of Arguments & Parameters.

--- Setup GitHub/Screencast_Functions_Practice/js/script.js
+++ Setup GitHub/Screencast_Functions_Practice/js/script.js
@@ -11,14 +11,12 @@
 
 
 
-calcArea(30,20);    //(Contains argument 1, argument 2)- Information being passed in.
+function dogYears(){            //Arguments needed. Information to send to the function.
+    var age         =   4;
+    var dogYears    =   age * 7; //Code ran by the function. Parameters.
 
-//Function Name is the function call.
-
-function calcArea(w, h){    //w = 30, h = 20 (This is parameter 1, parameter 2)(Storage container)
-    // The code that the function runs
-    var area    =   w * h;
-    console.log(area);
+    console.log("Sparky is " + dogYears + " years old.");
 }
-
+dogYears(); //Print above given parameters and arguments.
+dogYears();
 
