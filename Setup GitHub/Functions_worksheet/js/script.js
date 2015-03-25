e932ad37c7d5e95fa0aa8f3e67425d73fb7a1e8b
Linda Pemberton <school@lanwaves.biz>
2015-03-25 19:50:25 -0400

    Add Function Worksheet example of area in Basic Function
    Add Function Worksheet example of area in Basic Function.

--- Setup GitHub/Functions_worksheet/js/script.js
+++ Setup GitHub/Functions_worksheet/js/script.js
@@ -1,1 +1,32 @@
-alert("This file is connected!");
+//alert("This file is connected!");
+
+/**
+ Linda Pemberton
+ SDI Section 02
+ Functions Worksheet
+ March 24, 2015
+ */
+
+//Example: Calculate the area of a rectangle given the width and height of the rectangle.
+
+//Calculate the Area of a Rectangle - Functions, Arguments, Parameters, and Returning Values
+
+function calculateArea(){           //Function name assignment.
+    //
+    var width = 5;                  //Assign variable width of 5.
+    var height = 6;                 //Assign variable height of 6.
+    var area = width * height;      //Assign variable area to calculate width x height.
+    console.log("The Area of the Rectangle is " + area);
+}
+
+//This is considered invoking your function:
+calculateArea(); //Print console.log from function to invoke.  
+
+
+//calculateArea();
+
+
+//function calculateArea(w, h);
+ //   return w * h;
+
+
