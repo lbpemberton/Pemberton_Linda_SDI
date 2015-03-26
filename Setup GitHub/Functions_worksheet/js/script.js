ccf16f7df6d04329cec12e5c4834fbdff1050fb5
Linda Pemberton <school@lanwaves.biz>
2015-03-25 23:47:14 -0400

    Add circumference calculation using anonymous function.
    Add circumference calculation using anonymous function.  Functions
    worksheet calculation for circumference.

--- Setup GitHub/Functions_worksheet/js/script.js
+++ Setup GitHub/Functions_worksheet/js/script.js
@@ -7,22 +7,25 @@
  March 24, 2015
  */
 
-//CIRCUMFERENCE
+//CIRCUMFERENCE using Anonymous Functions
 
 //Calculate the circumference of a circle. - Functions, Arguments, Parameters, and Returning Values.
 
-var circleTotal       = circleCircum(9);        //Using this argument to send to parameters below:
 
-function circleCircum(radius) {                 //Parameter to be used for calculation.
+//Using Anonymous Function:
+var circleTotal = function(radius){        //Using this argument to send to parameters below:
+
+
+    //Parameter to be used for calculation.  Code the function runs:
     var circumference = 2 * radius * Math.PI;   //Variable for calculation needed.
 
 
 
     return circumference;                       //Return for variable set as circleTotal.
 }
-
-console.log("The circumference of the circle is " + circleTotal + ".");   //Console.log prints the return from
-// circleCircum function.
+var circle              = circleTotal(8);       //Invoking into circumference.
+console.log("The circumference of the circle is " + circle + ".");   //Console.log prints the return from
+// c
 
 
 
