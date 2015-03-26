a58183097d469969992bde7a60db2717cf9d2aa6
Linda Pemberton <school@lanwaves.biz>
2015-03-26 18:55:20 -0400

    Enter nameValidation function into Lottery Drawing
    Enter nameValidation function into Lottery Drawing.  Name needed to
    complete excercise.

--- Setup GitHub/Pemberton_Linda_Functions_Assignment/js/script.js
+++ Setup GitHub/Pemberton_Linda_Functions_Assignment/js/script.js
@@ -9,15 +9,40 @@
 
 //Florida Lottery Example
 
-//  variables
+//  variables needed for main code to run:
 
-var firstName       = prompt("Please enter your first name:");
-var age             = prompt("Please enter your age at time of ticket purchase:");
-var myLotteryPick;
+var firstName           = prompt("Please enter your first name:");                      //variable needed for function
+var age                 = prompt("Please enter your age at time of ticket purchase:");  //variable needed for function
+var myLotteryPick;                                                                      //variable needed for function
 
 
 
 //  functions
+
+
+
+function nameValidation(nameEntered){   //function named with argument
+    var numberAttempts  = 1;            //establish variable for function to establish attempts for entering name
+
+    while (nameEntered      === ""){    //parameters set for validation
+
+        //set up prompt to let player know that they have left the name slot empty
+        nameEntered     = prompt("Please enter your first name.\nDo not leave this blank.");
+
+
+        numberAttempts++;               //number attempts allowed + 1
+
+        if (numberAttempts  === 5) {    //if used to establish the most attempts allowed to enter name
+
+            //set up print message to show the player that they have reached the max number of name attempts
+            console.log("Alright, I see that you do not want to enter your name.");
+
+            break;                      //command tells code to stop making attempts
+        }
+    }
+
+    return nameEntered;  //return nameEntered into nameValidation function
+}
 
 
 
@@ -27,5 +52,9 @@ var myLotteryPick;
 
 //  main code
 
-console.log("Welcome " + firstName + ", have fun drawing your numbers!");
+//console.log("Welcome " + firstName + ", have fun drawing your numbers!");  //test to verify variable is printing
 console.log("Your age at the time of the drawing will be " + age + ".");
+
+//test to verify function is working properly:
+firstName = nameValidation(firstName);
+console.log(firstName);
