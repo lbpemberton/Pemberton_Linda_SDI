ab2989d86d64fd7c15abdc0e7e4d1dc1c04c93ba
Linda Pemberton <school@lanwaves.biz>
2015-03-25 21:33:39 -0400

    Using return within a function for Example in Functions Worksheet.
    Using return within a function for Example in Functions Worksheet.
    Area example using “return” within the function.

--- Setup GitHub/Functions_worksheet/js/script.js
+++ Setup GitHub/Functions_worksheet/js/script.js
@@ -11,16 +11,17 @@
 
 //Calculate the Area of a Rectangle - Functions, Arguments, Parameters, and Returning Values.
 
+var total = calculateArea(5,6);         //Assigning arguments to send to parameters below.
+
 function calculateArea(width, height){  //Width = 5 and height = 6 (Parameter 1 and 2)
 
 
-    var area = width * height;          //Assign variable area to calculate width x height.
-    console.log("The Area of the Rectangle is " + area);  //Result to print from function.
+    var area    = width * height;       //Assign variable area to calculate width x height.
+    return area;                        //Function spitting information out.  Function is invoked on line 14.
+
 }
 
-//This is considered invoking your function:
-calculateArea(5,6); //Print console.log from function to invoke. Assigns argument 1 and 2 into function.
-
+console.log("The area of the rectangle is " +total);//Print the return area of assigned variable "total".
 
 
 
