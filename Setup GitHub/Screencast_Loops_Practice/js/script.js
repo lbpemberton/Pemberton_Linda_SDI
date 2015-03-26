3221655f3033a4610d61c8c96b9102b232da19d5
Linda Pemberton <school@lanwaves.biz>
2015-03-26 03:04:47 -0400

    ADD While loops to practice.
    ADD While loops to practice.  Loops Screencast videos.

---
+++ Setup GitHub/Screencast_Loops_Practice/js/script.js
@@ -0,0 +1,18 @@
+//alert("Javascript works!");
+
+/**
+ * Created by lindapembertonweb on 3/26/15.
+ * Loops Screencast
+ */
+
+    // WHILE LOOP
+
+    
+var b = 10;     //sets up the index
+
+while (b > 0){  //checks the condition
+
+    console.log(b + " kegs on the wall.");
+    b--;        //increments or decrements the index
+
+}
