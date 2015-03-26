67f1585394f3c64f0679507091e06390c2294eee
Linda Pemberton <school@lanwaves.biz>
2015-03-26 03:12:00 -0400

    Add "Do While" loop to Loop Screencast for loops
    Add “Do / While” loop to Loop Screencast for loops.

--- Setup GitHub/Screencast_Loops_Practice/js/script.js
+++ Setup GitHub/Screencast_Loops_Practice/js/script.js
@@ -7,7 +7,7 @@
 
     // WHILE LOOP
 
-    
+
 var b = 10;     //sets up the index
 
 while (b > 0){  //checks the condition
@@ -16,3 +16,14 @@ while (b > 0){  //checks the condition
     b--;        //increments or decrements the index
 
 }
+
+    //DO WHILE LOOP - Less used!!
+
+var c = 10;
+
+do{     //run this code:
+    console.log(c + " kegs on the wall");
+    c--;
+
+}       //while this condition is true:
+while(c > 10);
