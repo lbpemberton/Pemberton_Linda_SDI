bab4aa71951beefae78d31b7ca66479f141bbd41
Linda Pemberton <school@lanwaves.biz>
2015-03-26 19:40:50 -0400

    Enter Function for age requirement in Functions Worksheet
    Enter Function for age requirement validation in Functions Assignment.
    Verify that age is entered for assignment.

--- Setup GitHub/Pemberton_Linda_Functions_Assignment/js/script.js
+++ Setup GitHub/Pemberton_Linda_Functions_Assignment/js/script.js
@@ -41,10 +41,33 @@ function nameValidation(nameEntered){   //function named with argument
         }
     }
 
-    return nameEntered;  //return nameEntered into nameValidation function
+    return nameEntered;                 //return nameEntered into nameValidation function
 }
 
+function ageRequirement(yearsOld){
 
+    var numberAttempts = 1;                //setup the index for the while loop
+
+    while (yearsOld         === ""){       //parameters set for validation of age entered
+
+        //set up prompt to let player know that they need to enter age when left blank
+        yearsOld        = prompt("Please enter your age.  This is important for your entry in the Florida Lottery.");
+
+        numberAttempts++;                   //number attempts allowed + 1
+
+        if (numberAttempts  === 5) {        //if used to establish the most attempts allowed to enter name
+
+            //set up print message to show the player that they have reached the max number of age entry attempts
+            console.log("It is clear that you do not want to enter your age. \nYou should know that Florida requires " +
+            "players are 18 years old to play.");
+
+            break;                          //command tells code to stop additional attempts to obtain age
+         }
+
+
+    }
+    return yearsOld;                        //return yearsOld into ageRequirement Function
+}
 
 
 
@@ -52,9 +75,13 @@ function nameValidation(nameEntered){   //function named with argument
 
 //  main code
 
-//console.log("Welcome " + firstName + ", have fun drawing your numbers!");  //test to verify variable is printing
-console.log("Your age at the time of the drawing will be " + age + ".");
+//console.log("Welcome " + firstName + ", have fun drawing your numbers!"); //test to verify variable is printing
+//console.log("Your age at the time of the drawing will be " + age + ".");  //test to verify variable age is printing
 
 //test to verify function is working properly:
-firstName = nameValidation(firstName);
+firstName   = nameValidation(firstName);
 console.log(firstName);
+
+//test to verify ageRequirement function is working properly:
+age         = ageRequirement(age);
+console.log(age);
